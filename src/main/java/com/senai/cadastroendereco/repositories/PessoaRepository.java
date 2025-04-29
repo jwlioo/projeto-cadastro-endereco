673a26eb6b3dbc38d9c36e4ab76f20eaac85e8c1
jwlioo <julio.botaccio@gmail.com>
2025-04-29 10:17:59 -0300

    Criaçao do Back-End Java

---
+++ src/main/java/com/senai/cadastroendereco/repositories/PessoaRepository.java
@@ -0,0 +1,10 @@
+package com.senai.cadastroendereco.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.senai.cadastroendereco.entities.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+		// busca a pessoa pelo email
+	Pessoa FindByEmail(String email);
+}
---
+++ src/main/java/com/senai/cadastroendereco/services/EnderecoService.java
@@ -0,0 +1,41 @@
+package com.senai.cadastroendereco.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senai.cadastroendereco.entities.Endereco;
+import com.senai.cadastroendereco.repositories.EnderecoRepository;
+
+@Service
+public class EnderecoService {
+
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	//Salvar um novo endereco
+	
+	public Endereco salvar(Endereco endereco) {
+		return enderecoRepository.save(endereco);
+	}
+	
+	//Buscar todos os endereços
+	
+	public List<Endereco> ListarTodos(){
+		return enderecoRepository.findAll();
+	}
+	
+	//Buscar um endereço por ID
+	
+	public Optional<Endereco> buscarPorId(Long id){
+		return enderecoRepository.findById(id);
+	}
+	//Deletar um endereço por ID
+	
+		public void deletarPorId(Long id) {
+			enderecoRepository.deleteById(id);
+		}
+}
